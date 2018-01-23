55ae091a45b830c03715797b507162b7a51b0280
AnkitAdmin <ankitadmin@Ankits-MacBook-Pro.local>
2018-01-23 14:06:13 +0530

    models has been created

---
+++ models/forms.js
@@ -0,0 +1,12 @@
+var mongoose = require('mongoose');
+var Schema = mongoose();
+
+var form = new Schema {
+    firstName: String,
+    lastName: String,
+    age: Number,
+    email: String,
+    password: String,
+}
+
+module.exports = mongoose.model('form', form);
